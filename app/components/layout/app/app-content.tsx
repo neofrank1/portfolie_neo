ec6ff57cfbf2144abbb74ad44f646a8757f3e88f
neofrankuy21 <neofrankuy21@gmail.com>
2025-11-22 20:34:02 +0800

    Header First Draft

--- app/components/layout/app/app-content.tsx
+++ app/components/layout/app/app-content.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function AppContent({ children }: { children: React.ReactNode }) {
     return (
-        <main>
+        <main className="mx-auto flex h-full w-full max-w-7xl flex-1 flex-col gap-4">
             {children}
         </main>
     );
--- app/components/layout/app/app-header.tsx
+++ app/components/layout/app/app-header.tsx
@@ -1,26 +1,40 @@
 import { NavigationMenu, NavigationMenuList, NavigationMenuItem } from "@/components/ui/navigation-menu";
+import Link from "next/link";
+import { type NavItem } from "@/types/nav_types";
+import { Linkedin, Facebook} from "lucide-react";
 
 export function AppHeader() {
+
+    const NavigationList: NavItem[] = [
+        { title: "About Me", href: "#about-me" },
+        { title: "Projects", href: "#projects" },
+        { title: "Contact", href: "#contact" },
+    ];
+
     return (
-        <header className="flex justify-around items-center h-16 border-b">
-            <div>
-                Logo
+        <header className="flex justify-around items-center h-16 border-b mb-4 bg-stone-950 text-white">
+            <div className="flex justify-center items-center">
+                <Link href="/" aria-label="Home">
+                    <img src="/neo_logo_clean.svg" alt="Neo logo" className="h-34 w-auto" />
+                </Link>
             </div>
-            <NavigationMenu className="h-full flex justify-center items-center gap-4">
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <a href="#about-me">About Me</a>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <a href="#projects">Projects</a>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <a href="#contact">Contact</a>
-                    </NavigationMenuItem>
+            <NavigationMenu>
+                <NavigationMenuList className="h-full flex justify-center items-center gap-6">
+                    {NavigationList.map((item, index) => (
+                        <NavigationMenuItem key={index}>
+                            <Link href={item.href} className="hover:text-slate-600 font-bold">{item.title}</Link>
+                        </NavigationMenuItem>
+                    ))}
                 </NavigationMenuList>
             </NavigationMenu>
-            <div>
-                Social
+            <div className="grid grid-cols-3 gap-5">
+                <Link href="https://www.linkedin.com/in/neo-frank-uy/" aria-label="LinkedIn profile">
+                    <Linkedin className="text-white fill-current" fill="currentColor" stroke="none" />
+                </Link>
+                <Link href="https://web.facebook.com/kratos9700/" aria-label="Facebook profile">
+                    <Facebook className="text-white fill-current" fill="currentColor" stroke="none"/>
+                </Link>
+                <Link href="#"></Link>
             </div>
         </header>
     );
